a0a46d7eaced34bdb43fe1297f4df1d32cede019
Student <student@compass.local>
2026-03-09 13:16:38 +0600

    add folders and ICountry interface

---
+++ src/types/country.ts
@@ -0,0 +1,12 @@
+export interface ICountry {
+  name: string;
+  alpha3Code: string;
+  flag: string;
+  capital: string;
+  region: string;
+  subregion: string;
+  population: number;
+  area: number;
+  nativeName: string;
+  numericCode: string;
+}
